bd0201d0cec8b989a000f6fbfffe724058436e84
vvJ <vv@vvJs-MacBook-Air.local>
2026-08-24 17:43:31 +0800

    refine: split homepage lettering across imagery

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,5 +1,18 @@
 import Image from 'next/image'
 
+function BrandLettering() {
+  return (
+    <>
+      <div className="showroom-cover__word showroom-cover__word--yuan">
+        {'YUAN'.split('').map((letter, index) => <span key={`${letter}-${index}`}>{letter}</span>)}
+      </div>
+      <div className="showroom-cover__word showroom-cover__word--showroom">
+        {'SHOWROOM'.split('').map((letter, index) => <span key={`${letter}-${index}`}>{letter}</span>)}
+      </div>
+    </>
+  )
+}
+
 export default function LocaleCoverPage() {
   return (
     <main className="showroom-cover">
@@ -15,13 +28,22 @@ export default function LocaleCoverPage() {
           <Image src="/images/showroom/home/flower-monochrome.png" alt="" fill sizes="17vw" />
         </div>
         <div className="showroom-cover__image showroom-cover__image--macro">
-          <Image src="/images/showroom/home/flower-macro.png" alt="" fill sizes="11vw" />
+          <Image src="/images/showroom/home/flower-macro.png" alt="" fill sizes="20vw" />
         </div>
-        <div className="showroom-cover__word showroom-cover__word--yuan">
-          {'YUAN'.split('').map((letter, index) => <span key={`${letter}-${index}`}>{letter}</span>)}
+        <div className="showroom-cover__letter-layer showroom-cover__letter-layer--base">
+          <BrandLettering />
         </div>
-        <div className="showroom-cover__word showroom-cover__word--showroom">
-          {'SHOWROOM'.split('').map((letter, index) => <span key={`${letter}-${index}`}>{letter}</span>)}
+        <div className="showroom-cover__letter-layer showroom-cover__letter-layer--image showroom-cover__letter-layer--fabric">
+          <BrandLettering />
+        </div>
+        <div className="showroom-cover__letter-layer showroom-cover__letter-layer--image showroom-cover__letter-layer--texture">
+          <BrandLettering />
+        </div>
+        <div className="showroom-cover__letter-layer showroom-cover__letter-layer--image showroom-cover__letter-layer--monochrome">
+          <BrandLettering />
+        </div>
+        <div className="showroom-cover__letter-layer showroom-cover__letter-layer--image showroom-cover__letter-layer--macro">
+          <BrandLettering />
         </div>
       </div>
     </main>
