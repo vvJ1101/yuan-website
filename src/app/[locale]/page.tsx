d688b6375d1704a378386e2ce09455aa538c7bc7
vvJ <vv@vvJs-MacBook-Air.local>
2026-08-24 17:33:01 +0800

    refine: strengthen editorial homepage composition

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -5,17 +5,17 @@ export default function LocaleCoverPage() {
     <main className="showroom-cover">
       <h1 className="sr-only">YUAN SHOWROOM</h1>
       <div className="showroom-cover__collage" aria-hidden="true">
-        <div className="showroom-cover__image showroom-cover__image--texture">
-          <Image src="/images/showroom/home/texture.png" alt="" fill sizes="34vw" priority />
+        <div className="showroom-cover__image showroom-cover__image--fabric">
+          <Image src="/images/showroom/home/generated-fabric-v1.png" alt="" fill sizes="42vw" priority />
         </div>
-        <div className="showroom-cover__image showroom-cover__image--paper">
-          <Image src="/images/showroom/home/paper-sculpture.png" alt="" fill sizes="28vw" priority />
+        <div className="showroom-cover__image showroom-cover__image--texture">
+          <Image src="/images/showroom/home/texture.png" alt="" fill sizes="18vw" priority />
         </div>
         <div className="showroom-cover__image showroom-cover__image--monochrome">
-          <Image src="/images/showroom/home/flower-monochrome.png" alt="" fill sizes="25vw" />
+          <Image src="/images/showroom/home/flower-monochrome.png" alt="" fill sizes="17vw" />
         </div>
         <div className="showroom-cover__image showroom-cover__image--macro">
-          <Image src="/images/showroom/home/flower-macro.png" alt="" fill sizes="18vw" />
+          <Image src="/images/showroom/home/flower-macro.png" alt="" fill sizes="11vw" />
         </div>
         <div className="showroom-cover__word showroom-cover__word--yuan">
           {'YUAN'.split('').map((letter, index) => <span key={`${letter}-${index}`}>{letter}</span>)}
